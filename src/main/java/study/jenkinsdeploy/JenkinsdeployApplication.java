d6c605e54a2b4cef968968a991e6f4f133dfd154
jemin <jemin03120111@gmail.com>
2022-05-31 00:30:03 +0900

    .

---
+++ src/main/java/study/jenkinsdeploy/JenkinsdeployApplication.java
@@ -0,0 +1,13 @@
+package study.jenkinsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsdeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/jenkinsdeploy/JenkinsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jenkinsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
